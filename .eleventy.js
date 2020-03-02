96a51eec2dba8d68dcbecedc82ba16c0a792e2b6
Peter deHaan <peter@deseloper.com>
2020-03-01 21:52:51 -0800

    Hhmm, sortings busted

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("docs", collection => {
-    const docs = collection.getFilteredByGlob("src/docs/*.njk").sort((a, b) => {
-      console.log(a.order, b.order);
-      return b.order - a.order;
-    });
-    console.log(docs.length);
-    console.log(docs)
+    const docs = collection.getFilteredByGlob("src/docs/*.njk")
+      .sort((a, b) => Number(a.data.order) - Number(b.data.order));
+    console.log(docs);
     return docs;
   });
   
